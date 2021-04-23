1ae140d711631c1ee673c6e9e8741dc2240ae7e9
buenofelipe1 <49288188+buenofelipe1@users.noreply.github.com>
2021-04-22 21:13:23 -0300

    refatoração

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading';
+
+
+import {
+  useFonts,
+  Jost_600SemiBold,
+  Jost_400Regular,
+} from '@expo-google-fonts/jost';
+
 
 export default function App(){
+
+  const [fontsLoaded] = useFonts({
+    Jost_600SemiBold,
+    Jost_400Regular
+  });
+
+  if(!fontsLoaded)
+  return <AppLoading />
+  
   return(
-    <>
+    
 
    <Welcome />
-   </>
+   
   )
 }
 
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,42 +1,60 @@
-import React, {useState} from 'react';
-import { Image, StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import wateringImg from '../../assets/watering.png';
-import { Button } from '../components/Button';
+import React from 'react';
+import { 
+    Image, 
+    StyleSheet, 
+    Text, 
+    Dimensions, 
+    SafeAreaView,
+    TouchableOpacity, 
+    View} 
+from 'react-native';
 
+import {Entypo} from '@expo/vector-icons';
+
+import wateringImg from '../../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/font';
 
 
 
 
 
 export function Welcome(){
-    const [visibily, setvisibily] = useState(false);
 
-    function handleVisibily(){
-        setvisibily(true)
-    }
 
     return(
         <SafeAreaView style={styles.container}>
-
+                <View style={styles.wraper}>
                 <Text style={styles.title}>
                 Gerencie 
                 {'\n'}
                 suas plantas de {'\n'}forma fácil
                 </Text>
 
-                {
-                    visibily &&
-                <Image source={wateringImg} style={ styles.image }/>
-                }
+              
+                <Image 
+                source={wateringImg} 
+                style={ styles.image}
+                resizeMode= "contain"
+                />
+                
 
                 <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. 
-                Nós cuidamos de lembrar você sempre que precisar.
+                    Não esqueça mais de regar suas plantas. 
+                    Nós cuidamos de lembrar você sempre que precisar.
                 </Text>
 
-                <Button title='Ver Imagem' onPress={handleVisibily} />
+                <TouchableOpacity 
+                style={styles.button} 
+                activeOpacity={0.7}
+                >
                 
+                    <Entypo 
+                        name="chevron-right" 
+                        style={styles.buttonIcon} 
+                    />
+                </TouchableOpacity>
+            </View>   
         </SafeAreaView>
     )
 }
@@ -44,27 +62,55 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         alignItems:'center'
     },
+    wraper:{
+        flex:1,
+        justifyContent: 'space-around',
+        alignItems:'center',
+        paddingHorizontal: 20,
+    },
     title:{
-        fontSize: 30,
+        fontSize: 28,
         fontWeight: 'bold',
         marginTop:38,
         textAlign: 'center',
-        color: colors.heading
+        color: colors.heading,
+       fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle:{
-        fontSize: 16,
+        fontSize: 18,
         paddingHorizontal: 20,
         textAlign: 'center',
         color: colors.heading,
+        fontFamily: fonts.text,
        
     },
    
     image:{
-        height:255,
-        width:255,
+        height: Dimensions.get('window').width *0.7
+        
+    },
+    button:{
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15,
+        marginBottom: 10,
+       
+        width:56,
+        height:56,
+    },
+    buttonText:{
+        fontSize:25,
+        padding:12,
+        color: colors.white
+    },
+    buttonIcon: {
+        fontSize:34,
+        color: colors.white,
         
     },
    
